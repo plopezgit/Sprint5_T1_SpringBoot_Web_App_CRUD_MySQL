cf5b8c59fecd9be0d0011c0bbd16ef807ee8d58c
Pedro Lopez <p.lopez.ch.hr@hotmail.com>
2024-01-10 23:08:06 +0100

    creates BranchService class

---
+++ src/main/java/cat/itacademy/barcelonactiva/lopez/pedro/s05/t01/n01/model/service/BranchService.java
@@ -0,0 +1,32 @@
+package cat.itacademy.barcelonactiva.lopez.pedro.s05.t01.n01.model.service;
+
+import cat.itacademy.barcelonactiva.lopez.pedro.s05.t01.n01.model.domain.Branch;
+
+import java.util.List;
+
+public class BranchService  implements BranchServiceInterface{
+    @Override
+    public Branch createBranch(Branch branch) {
+        return null;
+    }
+
+    @Override
+    public Branch updateBranch(Branch branch) {
+        return null;
+    }
+
+    @Override
+    public void deleteBranch(Integer id) {
+
+    }
+
+    @Override
+    public Branch getOneBranchById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Branch> getAllBranches() {
+        return null;
+    }
+}
