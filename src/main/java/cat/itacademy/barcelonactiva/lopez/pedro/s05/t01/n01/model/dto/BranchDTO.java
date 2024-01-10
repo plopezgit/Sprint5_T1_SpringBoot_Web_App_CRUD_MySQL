88d2ef934a9c22077bfdfbbc11b1d195300780c1
Pedro Lopez <p.lopez.ch.hr@hotmail.com>
2024-01-10 19:47:27 +0100

    creates basics BranchDTO class attributes based on requirements

--- src/main/java/cat/itacademy/barcelonactiva/lopez/pedro/s05/t01/n01/model/dto/BranchDTO.java
+++ src/main/java/cat/itacademy/barcelonactiva/lopez/pedro/s05/t01/n01/model/dto/BranchDTO.java
@@ -1,4 +1,18 @@
 package cat.itacademy.barcelonactiva.lopez.pedro.s05.t01.n01.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
 public class BranchDTO {
+    Integer id;
+    String name;
+    String country;
+    String type;
+    private List<String> europeanUnionCountriesReference;
 }
