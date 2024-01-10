ca588dbef8fa745da47d620dfdac3b8b342ff5b6
Pedro Lopez <p.lopez.ch.hr@hotmail.com>
2024-01-10 23:11:28 +0100

    injects BrandRepository dependency to BranchService class

--- src/main/java/cat/itacademy/barcelonactiva/lopez/pedro/s05/t01/n01/model/service/BranchService.java
+++ src/main/java/cat/itacademy/barcelonactiva/lopez/pedro/s05/t01/n01/model/service/BranchService.java
@@ -1,13 +1,18 @@
 package cat.itacademy.barcelonactiva.lopez.pedro.s05.t01.n01.model.service;
 
 import cat.itacademy.barcelonactiva.lopez.pedro.s05.t01.n01.model.domain.Branch;
+import cat.itacademy.barcelonactiva.lopez.pedro.s05.t01.n01.model.repository.BranchRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class BranchService  implements BranchServiceInterface{
-    @Override
+
+    @Autowired
+    private BranchRepository branchRepository;
+
     public Branch createBranch(Branch branch) {
         return null;
     }
