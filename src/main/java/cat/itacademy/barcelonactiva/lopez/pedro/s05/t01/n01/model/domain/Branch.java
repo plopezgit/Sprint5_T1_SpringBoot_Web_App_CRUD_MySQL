d42466fdc3b58aef0d0e0ab6df6df1b747f6f649
Pedro Lopez <p.lopez.ch.hr@hotmail.com>
2024-01-12 13:13:50 +0100

    develops BranchDTO class and updates Branch entity class; updates messages.properties setup

--- src/main/java/cat/itacademy/barcelonactiva/lopez/pedro/s05/t01/n01/model/domain/Branch.java
+++ src/main/java/cat/itacademy/barcelonactiva/lopez/pedro/s05/t01/n01/model/domain/Branch.java
@@ -2,24 +2,21 @@ package cat.itacademy.barcelonactiva.lopez.pedro.s05.t01.n01.model.domain;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import java.io.Serializable;
 
 @Data
+@NoArgsConstructor
 @Entity
 @Table(name="branches")
 public class Branch implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer id;
-    @NotEmpty
     String name;
-    @NotEmpty
     String country;
-
-    public Branch() {
-    }
 
     public Branch(String name, String country) {
         this.name = name;
--- src/main/java/cat/itacademy/barcelonactiva/lopez/pedro/s05/t01/n01/model/dto/BranchDTO.java
+++ src/main/java/cat/itacademy/barcelonactiva/lopez/pedro/s05/t01/n01/model/dto/BranchDTO.java
@@ -4,25 +4,36 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.NotEmpty;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@Data
+@NoArgsConstructor
 public class BranchDTO {
     private Integer id;
+    @NotEmpty
     String name;
+    @NotEmpty
     String country;
     String type;
-    private final List<String> EUROPEAN_COUNTRIES_REFERENCE;
+    private final List<String> EUROPEAN_COUNTRIES_REFERENCE = List.of("Austria", "Belgium", "Bulgaria", "Croatia", "Republic of Cyprus",
+            "Czech Republic", "Denmark", "Estonia", "Finland", "France", "Germany",
+            "Greece", "Hungary", "Ireland", "Italy", "Latvia", "Lithuania", "Luxembourg",
+            "Malta", "Netherlands", "Poland", "Portugal", "Romania", "Slovakia",
+            "Slovenia", "Spain and Sweden");
 
-    public BranchDTO () {
-        EUROPEAN_COUNTRIES_REFERENCE = new ArrayList<>(Arrays
-                .asList("Austria", "Belgium", "Bulgaria", "Croatia", "Republic of Cyprus",
-                        "Czech Republic", "Denmark", "Estonia", "Finland", "France", "Germany",
-                        "Greece", "Hungary", "Ireland", "Italy", "Latvia", "Lithuania", "Luxembourg",
-                        "Malta", "Netherlands", "Poland", "Portugal", "Romania", "Slovakia",
-                        "Slovenia", "Spain and Sweden"));
+    public BranchDTO(String name, String country, String type) {
+        this.name = name;
+        this.country = country;
+        this.type = type;
+    }
+
+    private void setBranchType() {
+        this.type = EUROPEAN_COUNTRIES_REFERENCE.stream().anyMatch(country -> country
+                .equalsIgnoreCase(this.country))? "Euro" : "Not Euro";
     }
 }
