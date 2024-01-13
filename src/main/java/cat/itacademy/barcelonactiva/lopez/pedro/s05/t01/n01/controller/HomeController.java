52986f43d98764b67eea794b86f350a272f62aaa
Pedro Lopez <p.lopez.ch.hr@hotmail.com>
2024-01-13 20:19:50 +0100

    updates HomeController and index.html files

---
+++ src/main/java/cat/itacademy/barcelonactiva/lopez/pedro/s05/t01/n01/controller/HomeController.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.lopez.pedro.s05.t01.n01.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping({"/index","/home","/"})
+    public String index () {
+        return "index";
+    }
+}
